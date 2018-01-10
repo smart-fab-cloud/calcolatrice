6ff93c8795d604face5c56111bc57bcdb3e4ba12
Jacopo Soldani <jsoldani@gmail.com>
2018-01-10 08:28:09 +0100

    engineered version

--- src/main/java/ifts/calcolatrice/Calcolatrice.java
+++ src/main/java/ifts/calcolatrice/Calcolatrice.java
@@ -1,10 +1,12 @@
 package ifts.calcolatrice;
 
+import java.util.Optional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/calcolatrice")
 @Produces(MediaType.APPLICATION_JSON)
@@ -14,37 +16,79 @@ public class Calcolatrice {
     
     @GET
     @Path("/somma")
-    public Somma somma(
-        @QueryParam("n") int n,
-        @QueryParam("m") int m
+    public Response somma(
+        @QueryParam("n") Optional<Integer> n,
+        @QueryParam("m") Optional<Integer>  m
     ) {
-        return new Somma(n,m,n+m);
+        if(!n.isPresent() || !m.isPresent())
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("'n' e 'm' devono essere entrambi indicati.")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+
+        int n1 = n.get();
+        int n2 = m.get();
+
+        return Response.ok().entity(new Somma(n1,n2,n1+n2)).build();
     }
 
     @GET
     @Path("/differenza")
-    public Differenza differenza(
-        @QueryParam("n") int n,
-        @QueryParam("m") int m
+    public Response differenza(
+        @QueryParam("n") Optional<Integer> n,
+        @QueryParam("m") Optional<Integer>  m
     ) {
-        return new Differenza(n,m,n-m);
+        if(!n.isPresent() || !m.isPresent())
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("'n' e 'm' devono essere entrambi indicati.")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        
+        int n1 = n.get();
+        int n2 = m.get();
+
+        return Response.ok().entity(new Differenza(n1,n2,n1-n2)).build();
     }
 
     @GET
     @Path("/prodotto")
-    public Prodotto prodotto(
-        @QueryParam("n") int n,
-        @QueryParam("m") int m
+    public Response prodotto(
+        @QueryParam("n") Optional<Integer> n,
+        @QueryParam("m") Optional<Integer>  m
     ) {
-        return new Prodotto(n,m,n*m);
+        if(!n.isPresent() || !m.isPresent())
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("'n' e 'm' devono essere entrambi indicati.")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        
+        int n1 = n.get();
+        int n2 = m.get();
+        
+        return Response.ok().entity(new Prodotto(n1,n2,n1*n2)).build();
     }
 
     @GET
     @Path("/divisione")
-    public Quoziente divisione(
-        @QueryParam("n") int n,
-        @QueryParam("m") int m
+    public Response divisione(
+        @QueryParam("n") Optional<Integer> n,
+        @QueryParam("m") Optional<Integer>  m
     ) {
-        return new Quoziente(n,m,n/m,n%m);
+        if(!n.isPresent() || !m.isPresent())
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("'n' e 'm' devono essere entrambi indicati.")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        
+        int n1 = n.get();
+        int n2 = m.get();
+        
+        if(n2 == 0)
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("'m' non può essere zero.")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        
+        return Response.ok().entity(new Quoziente(n1,n2,n1/n2,n1%n2)).build();
     }
 }
